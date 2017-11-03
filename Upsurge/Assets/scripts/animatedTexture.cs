80138665d76906fcaa73e7d876e5a4661766b465
Your Name <you@example.com>
2017-11-03 17:34:24 -0400

    first commit

---
+++ Upsurge/Assets/scripts/animatedTexture.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animatedTexture : MonoBehaviour {
+	public Vector2 speed = Vector2.zero; 
+	private Vector2 offset = Vector2.zero;
+	private Material material; 
+
+
+	// Use this for initialization
+	void Start () {
+		material = GetComponent<Renderer>().material;
+		offset = material.GetTextureOffset("_MainTex");		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		offset += speed * Time.deltaTime;
+		material.SetTextureOffset("_MainTex", offset);
+	}
+}
---
+++ Upsurge/Assets/scripts/cameraScaling.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraScaling : MonoBehaviour {
+
+	public static float pixelToUnits =1f;
+	public static float scale = 1f; 
+	public Vector2 nativeResolution = new Vector2(240,160);
+
+	// Use this for initialization
+	void  Awake () {
+		// Reference the camera
+		var camera= GetComponent<Camera>();
+		
+		if(camera.orthographic){
+			scale = Screen.height/nativeResolution.y;
+			pixelToUnits *= scale;
+			camera.orthographicSize = (Screen.height / 2.0f)/pixelToUnits;
+		}		
+	}
+	
+
+}
